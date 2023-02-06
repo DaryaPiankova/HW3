7ca91ab30e96f4a0eccca1ece27ecc93c6b9169b
Daria <darya.piankova@yandex.ru>
2023-02-06 17:44:27 +0500

    Tsk 19

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.WriteLine("Введите пятизначное число");
+string a=Console.ReadLine();
+int count=0;
+for(int i=0; i<(a.Length/2); i++){
+    if(a[i]!=a[a.Length-i-1]){
+       count++;        
+    }
+}
+if(count==0){
+    Console.WriteLine("Да");
+}else{
+    Console.WriteLine("Нет");
+}
